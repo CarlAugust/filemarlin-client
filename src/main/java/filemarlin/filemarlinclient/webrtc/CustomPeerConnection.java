f59eba28847df35eeb865b6cfb4a5beab6a1c157
DumbAugust <carlaugust.sponberg@gmail.com>
2026-09-13 20:34:11 +0200

    CustomPeerConnection class to couple datachannel and peerConnection together.

---
+++ src/main/java/filemarlin/filemarlinclient/webrtc/CustomPeerConnection.java
@@ -0,0 +1,65 @@
+package filemarlin.filemarlinclient.webrtc;
+
+
+import dev.onvoid.webrtc.*;
+import tools.jackson.databind.ObjectMapper;
+
+public class CustomPeerConnection {
+
+    private WebRTCManager manager;
+    private RTCPeerConnection peerConnection;
+    private RTCDataChannel dataChannel;
+
+    private RTCConfiguration config;
+    private PeerConnectionFactory factory;
+    private RTCOfferOptions options;
+
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public CustomPeerConnection(
+            String id,
+            RTCConfiguration config,
+            PeerConnectionFactory factory,
+            RTCOfferOptions options) {
+
+        this.config = config;
+        this.factory = factory;
+        this.options = options;
+
+        createPeerConnection(id);
+        createOffer(id);
+    }
+
+    public void createPeerConnection(String id) {
+
+        var peerConnection = factory.createPeerConnection(config, new PeerConnectionObserver() {
+
+            @Override
+            public void onIceCandidate(RTCIceCandidate candidate) {
+                var signalPayload = objectMapper.valueToTree(candidate);
+                // Signaller.sendSignal(id, candidate);
+            }
+
+            @Override
+            public void onDataChannel(RTCDataChannel dataChannel) {
+                PeerConnectionObserver.super.onDataChannel(dataChannel);
+            }
+        });
+        var dataChannel = peerConnection.createDataChannel("Data", new RTCDataChannelInit());
+    }
+
+    public void createOffer(String id) throws NullPointerException {
+        peerConnection.createOffer(options, new CreateSessionDescriptionObserver() {
+            @Override
+            public void onSuccess(RTCSessionDescription description) {
+                // Signaller.sendSignal(id, description);
+            }
+
+            @Override
+            public void onFailure(String error) {
+                System.err.println("Failed to set descriptor: " + error);
+            }
+        });
+    }
+}
--- src/main/java/filemarlin/filemarlinclient/webrtc/WebRTCManager.java
+++ src/main/java/filemarlin/filemarlinclient/webrtc/WebRTCManager.java
@@ -1,18 +1,24 @@
 package filemarlin.filemarlinclient.webrtc;
 
 import dev.onvoid.webrtc.*;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebRTCManager {
     private final RTCConfiguration config = new RTCConfiguration();
     private final RTCIceServer iceServer = new RTCIceServer();
     private final PeerConnectionFactory factory = new PeerConnectionFactory();
+    private final RTCOfferOptions options = new RTCOfferOptions();
+    private final Map<String, CustomPeerConnection> peerConnectionsMap = new ConcurrentHashMap<>();
 
     public WebRTCManager() {
         iceServer.urls.add("stun:stun.l.google.com:19302");
+        config.iceServers.add(iceServer);
+    }
 
-        // Lets just test first of all
-
-        RTCPeerConnection peerConnection = factory.createPeerConnection(config, new PeerConnectionObserver());
-
+    public void establishConnection(String id) {
+        peerConnectionsMap.put(id, new CustomPeerConnection(id, config, factory, options));
     }
 }
